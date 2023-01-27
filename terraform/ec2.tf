aac7aa20d31c549981a1a757f06b365de82afe68
nok911 <niall.okeeffe@weareams.com>
2023-01-27 12:07:35 +0000

    key change

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "group-two-server" {
    ami           = "ami-0c68b55d1c875067e"
    instance_type = "t3.medium"
    availability_zone = "eu-west-1a"
-   key_name = "AMSKey"
+   key_name = "petclinic"
 
    network_interface {
      # device index (required)
